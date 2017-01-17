57291cfdc22656734d7098ba995f5ee2a822b7be
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-17 19:34:48 +0000

    completed 'initializing your project

--- request-as-json.js
+++ request-as-json.js
@@ -1,1 +1,38 @@
 //Created the "request-as-json.js" file
+
+var request = require('request');
+var prompt = require('prompt');
+
+//var url = "https://maps.googleapis.com/maps/api/geocode/json?address=montreal";
+var url = "https://maps.gogeais.com/aps/ai/geocode/json?address=montreal";
+
+
+
+function requestJson(url, callback) {
+    request(url, function(err, result) {
+        if (err) {
+            callback(err);
+        }
+        else {
+            try {
+                callback(
+                    null, 
+                    JSON.parse(result.body)
+                );
+            }
+            catch (err) {
+                console.log("something went wrong in JSON request");
+                callback(err);
+            }
+        }
+    })
+}
+
+requestJson(url, function(err, result){
+    if (err){
+        console.log("THIS IS THE ERROR " ,err);
+    }
+    else{
+        console.log("this is the result " , result);
+    }
+})
