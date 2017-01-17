8753f61fb5e96d6f1078a46d2b0dcab0b1cdfabb
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-17 20:43:57 +0000

    Weather file created for dark sky questions

---
+++ weather.js
@@ -0,0 +1,6 @@
+var requestJSON = require('./library/request-json');
+
+var urlGoogle = "https://maps.googleapis.com/maps/api/geocode/json?address=montreal";
+var urlDarkSky = "https://api.darksky.net/forecast/2c409eb3da49a3706de9fe983f4b9781/37.8267,-122.4233";
+
+var
