7f29ec9e83c23b21730975496d5dbaa1dde6d2bd
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-17 21:24:36 +0000

    made progress on weather app

--- weather.js
+++ weather.js
@@ -1,6 +1,32 @@
 var requestJSON = require('./library/request-json');
+var prompt = require('prompt');
 
-var urlGoogle = "https://maps.googleapis.com/maps/api/geocode/json?address=montreal";
-var urlDarkSky = "https://api.darksky.net/forecast/2c409eb3da49a3706de9fe983f4b9781/37.8267,-122.4233";
-
-var
+prompt.get('city', function(err, result){
+    if(err){
+        console.log("Something went wrong with the 'prompt'");
+    }
+    else{
+        var urlCity = "https://maps.googleapis.com/maps/api/geocode/json?address=" + result.city;
+        
+        requestJSON(urlCity, function(err, cityInfo){
+            if(err){
+                console.log("This is the error", err);
+            }
+            else{
+                var cityLat = cityInfo.results[0].geometry.location.lat;
+                var cityLng = cityInfo.results[0].geometry.location.lng;
+                
+                var urlDarkSky = "https://api.darksky.net/forecast/2c409eb3da49a3706de9fe983f4b9781/" + cityLat + cityLng;
+                
+                requestJSON(urlDarkSKy, function(err, darkCityInfo){
+                    if(err){
+                        console.log("This is the error ", err);
+                    }
+                    else{
+                        
+                    }
+                })
+            }
+        })
+    }
+})
